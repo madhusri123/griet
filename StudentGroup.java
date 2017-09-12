2e8c70c1ff759e0ca7f64a676ead02b1a4a11092
madhusri123 <31862945+madhusri123@users.noreply.github.com>
2017-09-12 13:04:36 +0530

    Add files via upload

---
+++ StudentGroup.java
@@ -0,0 +1,139 @@
+import java.util.*;
+import java.Collections;
+import java.io.*;
+
+
+ 
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+	
+	/**
+	 * DO NOT remove or change this constructor, it will be used during task check
+	 * @param length
+	 */
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents()
+ {
+	if((s.length)>0)
+{
+	for(int i=0;i<=length;i++)
+{
+Scanner sc=new Scanner(sc.next());
+System.out.println(sc.next());
+}
+}
+else 
+
+
+		return null;
+	}
+
+	@Override
+	public void setStudents(Student[] students)
+ {
+		try{
+                    if(students.length<0||students.length>s.length)
+{
+
+}
+}
+catch(Exception e);
+{
+System.out.println("caught"+e.printStackTrace());
+}
+}
+}
+
+
+
+	@Override
+	public Student getStudent(int index) {
+try{
+if(s.length()>0)
+{
+           student[index]=s.index;
+       }
+}
+else
+		
+		return null;
+catch(Exception e)
+{
+System.out.println("caught"+e.printstackTrace());
+	}
+}
+
+
+	@Override
+	public void setStudent(Student student, int index) {
+try{
+if(s.length()>0)
+{
+           student[index]=s.index;
+       }
+
+else
+		
+		return null;
+}
+catch(Exception e){
+System.out.println("caught"+e.printstackTrace());
+}
+
+		
+
+	@Override
+	public void addFirst(Student student) {
+try{
+		StringBuffer sb=new StringBuffer();
+                System.out.println(sb+"student[]");
+	}
+catch(IllegalArgumentException)
+{
+System.out.println("caught"+e.printstackTrace());
+}
+	@Override
+	public void addLast(Student student) {
+try{
+		StringBuffer sb1=new StringBuffer();
+                System.out.println("student[]"+sb1);
+}
+
+
+	@Override
+	public void add(Student student, int index) {
+		// Add your implementation here
+	ArrayList<integer>=new ArrayList()
+       Iterator itr=new Iterator();
+                ArrayList.add(sc.nextInt())
+while(itr.hasnext())
+{
+System.out.println(itr.next());
+}
+catch(IllegalArgumentException)
+{
+System.out.println("caught"+e.printstackTrace());
+}
+}
+	
+	
+	 void bubbleSort() {
+		for(int i=0;i<s.length();i++)
+{
+for(int j=0;j<s.length-1;j++)
+{
+if(s[j]>s[j+1])
+{
+temp=s[j];
+s[j]=s[j+1];
+s[j+1]=temp;
+	}
+
+	
+	
+}
